0cd6c9829634cae2873d4da49ed5738e9d4e4c43
DAKSH72 <75013866+DAKSH72@users.noreply.github.com>
2020-12-11 14:52:35 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,31 @@
+var fixedRect,movingRect;
+
+
+function setup() {
+  createCanvas(800,400);
+  fixedRect=createSprite(400, 200, 50, 50);
+  fixedRect.shapeColor="blue";
+  movingRect=createSprite(200, 200, 80, 30);
+  movingRect.shapeColor="blue";
+}
+
+function draw() {
+  background(0);  
+  movingRect.x=mouseX;
+  movingRect.y=mouseY;
+  console.log(movingRect.x-fixedRect.x)
+  console.log(fixedRect.width/2+movingRect.width/2)
+if(movingRect.x-fixedRect.x<fixedRect.width/2+movingRect.width/2&&
+  fixedRect.x-movingRect.x<fixedRect.width/2+movingRect.width/2&&
+  movingRect.y-fixedRect.y<fixedRect.height/2+movingRect.height/2&&
+  fixedRect.y-movingRect.y<fixedRect.height/2+movingRect.height/2){
+  fixedRect.shapeColor="red";
+  movingRect.shapeColor="red";
+}
+else{
+  fixedRect.shapeColor="blue";
+  movingRect.shapeColor="blue";
+}
+
+  drawSprites();
+}
